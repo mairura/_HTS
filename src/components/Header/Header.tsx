a50b7a08106fcc99dfb0236d9adcd136c20d8f7e
ted <mairurat@gmail.com>
2024-01-08 22:17:33 +0300

    first commit

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import { FaUserCircle } from "react-icons/fa";
+import { MdDarkMode } from "react-icons/md";
+
+const Header = () => {
+  return (
+    <header className="py-10 px-4 container mx-auto text-xl flex flex-wrap md:flex-nowrap items-center justify-between">
+      <div className="flex items-center w-full md:2/3">
+        <Link href="/" className="font-black text=[#F27405]">
+          Hotelzz
+        </Link>
+
+        <ul className="flex items-center ml-5">
+          <li>
+            <Link href="/auth">
+              {" "}
+              <FaUserCircle className="cursor-pointer" />{" "}
+            </Link>
+          </li>
+
+          <li className="ml-2">
+            <MdDarkMode />
+          </li>
+        </ul>
+      </div>
+
+      <ul></ul>
+    </header>
+  );
+};
+
+export default Header;
