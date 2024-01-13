81317344dbde5a4b545081ca3c8b0d0c889ab306
ted <mairurat@gmail.com>
2024-01-13 21:24:02 +0300

    implemented all sanity schemas

---
+++ schemas/review.ts
@@ -0,0 +1,39 @@
+import { decodeAction } from "next/dist/server/app-render/entry-base";
+import { defineField } from "sanity";
+
+const review = {
+  name: "review",
+  title: "Review",
+  type: "document",
+  fields: [
+    defineField({
+      name: "user",
+      title: "User",
+      type: "reference",
+      to: [{ type: "user" }],
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: "hotelRoom",
+      title: "Hotel Room",
+      type: "reference",
+      to: [{ type: "hotelRoom" }],
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: "text",
+      title: "Review Text",
+      type: "text",
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: "userRating",
+      title: "User Rating",
+      type: "number",
+      validation: (Rule) =>
+        Rule.required().min(1).max(5).error("Rating must be between 1 and 5"),
+    }),
+  ],
+};
+
+export default review;
