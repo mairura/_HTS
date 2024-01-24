93d2a23a8f71f0e4ba00394034b33c2e2e5749ce
ted <mairurat@gmail.com>
2024-01-24 23:18:31 +0300

    added hoel services in roomdetails page

--- src/app/(web)/rooms/[slug]/page.tsx
+++ src/app/(web)/rooms/[slug]/page.tsx
@@ -1,6 +1,10 @@
 "use client";
 
 import { getRoom } from "@/libs/apis";
+import { MdOutlineCleaningServices } from "react-icons/md";
+import { LiaFireExtinguisherSolid } from "react-icons/lia";
+import { AiOutlineMedicineBox } from "react-icons/ai";
+import { GiSmokeBomb } from "react-icons/gi";
 import useSWR from "swr";
 import LoadingSpinner from "../../loading";
 import HotelPhotoGallery from "@/components/HotelPhotoGallery/HotelPhotoGallery";
@@ -35,12 +39,12 @@ const RoomDetails = (props: { params: { slug: string } }) => {
                 {room.name} - {room.dimension}
               </h2>
               <div className="flex my-11">
-                {room?.offeredAmenities?.map(ammenity => (
+                {room?.offeredAmenities?.map((ammenity) => (
                   <div
                     key={ammenity._key}
                     className="md:w-44 w-fit text-center px-2 md:px-0 h-20 md:h-40 mr-3 bg-[#eff0f2] dark:bg-gray-800 rounded-lg grid place-content-center"
                   >
-                    <i className={`fa-solid ${ammenity.icon} md:text-2xl`} ></i>
+                    <i className={`fa-solid ${ammenity.icon} md:text-2xl`}></i>
                     <p className="text-xs md:text-base pt-3">
                       {ammenity.amenity}
                     </p>
@@ -50,6 +54,48 @@ const RoomDetails = (props: { params: { slug: string } }) => {
               <div className="mb-11">
                 <h2 className="font-bold text-3xl mb-2">Description</h2>
                 <p>{room.description}</p>
+              </div>
+              <div className="mb-11">
+                <h2 className="font-bold text-3xl mb-2">Offered Ammenities</h2>
+                <div className="grid grid-cols-2">
+                  {room?.offeredAmenities?.map((ammenity) => (
+                    <div
+                      key={ammenity._key}
+                      className="flex items-center md:my-3 my-1"
+                    >
+                      <i className={`fa-solid ${ammenity.icon}`}></i>
+                      <p className="text-xs md:text-base ml-3">
+                        {ammenity.amenity}
+                      </p>
+                    </div>
+                  ))}
+                </div>
+              </div>
+
+              <div className="mb-11">
+                <h2 className="font-bold text-3xl mb-2">Safety And Hygiene</h2>
+                <div className="grid grid-cols-2">
+                  <div className="flex items-center my-1 md:my-3">
+                    <MdOutlineCleaningServices />
+                    <p className="ml-2 md:text-base text-xs">Daily Cleaning</p>
+                  </div>
+                  <div className="flex items-center my-1 md:my-3">
+                    <LiaFireExtinguisherSolid />
+                    <p className="ml-2 md:text-base text-xs">
+                      Fire Extinguishers
+                    </p>
+                  </div>
+                  <div className="flex items-center my-1 md:my-3">
+                    <AiOutlineMedicineBox />
+                    <p className="ml-2 md:text-base text-xs">
+                      Disinfections and Sterilizations
+                    </p>
+                  </div>
+                  <div className="flex items-center my-1 md:my-3">
+                    <GiSmokeBomb />
+                    <p className="ml-2 md:text-base text-xs">Smoke Detectors</p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
